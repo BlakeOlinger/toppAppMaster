3fcb2f625b23a9a4f3a68bf8bad0fa2a5f526eef
Blake Olinger <blake.olinger@symbosoft.com>
2019-06-28 10:32:37 -0400

    Initial commit

---
+++ src/com/practice/Main.java
@@ -0,0 +1,18 @@
+package com.practice;
+
+/*
+this microservice daemon is the master controller for the other microservices:
+the GUI, DB, Updater and C# SW
+
+This is called from the shortcut and  starts the other services.
+
+this will receive commands via the master.config file and issue out as needed to
+other services
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
