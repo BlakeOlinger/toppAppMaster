f306b62baa76d9725f56deab3009b80b608e9e21
Blake Olinger <b.olinger.software.solutions.1101@gmail.com>
2019-06-30 18:26:29 -0400

    -- updated Master Database Kill command to include a '1' for push state

--- src/com/practice/DBProgramState.java
+++ src/com/practice/DBProgramState.java
@@ -28,32 +28,6 @@ class DBProgramState implements Runnable{
      */
     @Override
     public void run() {
-        // TODO get to write to file - won't for some reason - make DB daemon check more often for kill
-        // decouple check for kill from check for pull
-        // TODO  - change non DB files from toppAppDBdaemon to  /programfiles/config and
-        // /programfiles/session - for .config and .session files
-        // TODO pathing off in DB daemon - created multiple .git repos
-        // TODO have master boot strap app with installer function - master will have shortcut icon
-        // move DB initializer from DB daemon to master and master will copy .jars and run them
-        // make shortcut on desktop as well
-        // TODO - restructure modules/packages so all apps are in one package for easy JLinking
-        // TODO - JLink all so each service is a standalone entity
-        // TODO - have master listen for update service command to update the updater service
-        // TODO - change any unnecessary calls to cmd.exe to Java runtime commands
-        // TODO - include .jar/.bat in DB /bin folder
-        // TODO - decouple DB daemon source code repository from DB repository
-        // TODO - in addition to an installer - make an uninstaller via some
-        // manual command input in the master.config file - make it delete all
-        // directories and files related to app
-        // TODO - either for GUI or internally allow a user to input a general area
-        // for a new feature - I, II, III, IV - centered in each quadrant
-        // and I-IV, II-III, III-IV, IV-I for centered on the right/left, up/down axis
-        // TODO - master.installer in initializing DB is going to check for git command
-        // functionality and git repo - if not install - installer ships with master
-        // configure PATH for git and then initialize repo
-        // TODO - make tool to copy .jar/.bat from restricted dev out folder to TOPP App DB update folder
-        // add that tool to PATH so a simple command will execute it
-
 
         System.out.println(" Sending Kill Command - Database Microservice");
 
@@ -61,6 +35,7 @@ class DBProgramState implements Runnable{
         var DBconfigRead = new FileInputStream(Config.configFilePath + "DBdaemon.config")) {
             char command = '1';
 
+            DBconfig.write((int) command);
             DBconfig.write((int) command);
 
             int byteRead = DBconfigRead.read();
