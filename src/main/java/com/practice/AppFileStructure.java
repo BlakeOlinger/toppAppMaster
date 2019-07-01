cdaa6eb1989dbcaac6e4b6a8ada40caaaa1dcf6c
Blake Olinger <blake.olinger@symbosoft.com>
2019-07-01 15:21:44 -0400

    -- cleaned up file and directory installer

--- src/main/java/com/practice/AppFileStructure.java
+++ src/main/java/com/practice/AppFileStructure.java
@@ -13,40 +13,18 @@ class AppFileStructure implements Runnable{
         thread.start();
     }
 
-    /**
-     * When an object implementing interface <code>Runnable</code> is used
-     * to create a thread, starting the thread causes the object's
-     * <code>checkAndInitializeDB</code> method to be called in that separately executing
-     * thread.
-     * <p>
-     * The general contract of the method <code>checkAndInitializeDB</code> is that it may
-     * take any action whatsoever.
-     *
-     * @see Thread#run()
-     */
     @Override
     public void run() {
         var configPath = new File("./programFiles/config/");
         var currentVersion = new File("./programFiles/bin/currentVersion/");
 
-        System.out.println(" Checking for existing File Directories");
         if(!configPath.exists()) {
-            System.out.println(" Program Directories Not Found - Creating Directories");
 
             if(!currentVersion.exists()) {
                 currentVersion.mkdirs();
             }
 
             configPath.mkdirs();
-
-            if(configPath.exists() && currentVersion.exists()) {
-                System.out.println(" Directories Successfully Made");
-            } else {
-                System.out.println(" Unable to Create Directories");
-                ++Config.errorCount;
-            }
-        } else {
-            System.out.println(" Program Directories Found");
         }
 
     }
--- src/main/java/com/practice/Main.java
+++ src/main/java/com/practice/Main.java
@@ -1,6 +1,5 @@
 package com.practice;
 
-
 /*
 this microservice daemon is the master controller for the other microservices:
 the GUI, DB, Updater and C# SW
@@ -10,11 +9,13 @@ This is called from the shortcut and  starts the other services.
 this will receive commands via the master.config file and issue out as needed to
 other services
  */
+
 public class Main {
 
     public static void main(String[] args) {
-
         InitializeDB.checkAndInitializeDB();
+
+        new AppFileStructure().checkAndInstallFileDirectories();
 
       //   Installer.checkAndInstall();
 
