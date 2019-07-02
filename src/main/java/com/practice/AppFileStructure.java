732a8f931a69482fa0108bd06b3379b0df4b9426
Blake Olinger <blake.olinger@symbosoft.com>
2019-07-02 14:28:50 -0400

    -- added blemp directory to initalization sequence

--- src/main/java/com/practice/AppFileStructure.java
+++ src/main/java/com/practice/AppFileStructure.java
@@ -17,15 +17,18 @@ class AppFileStructure implements Runnable{
     public void run() {
         var configPath = new File("./programFiles/config/");
         var currentVersion = new File("./programFiles/bin/currentVersion/");
+        var blempDirectory = new File("./programFiles/blemp/");
 
         if(!configPath.exists()) {
-
-            if(!currentVersion.exists()) {
-                currentVersion.mkdirs();
-            }
-
             configPath.mkdirs();
         }
 
+        if(!currentVersion.exists()) {
+            currentVersion.mkdirs();
+        }
+
+        if(!blempDirectory.exists()) {
+            blempDirectory.mkdirs();
+        }
     }
 }
